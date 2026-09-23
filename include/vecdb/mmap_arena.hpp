f7ba63e9fff64e4f763a9f4a87c77e1f935496bb
Lalitha <lalithab010907@gmail.com>
2026-09-23 17:19:19 +0530

    Format MmapArena files

--- include/vecdb/mmap_arena.hpp
+++ include/vecdb/mmap_arena.hpp
@@ -4,44 +4,40 @@
 #include <filesystem>
 #include <memory>
 
-namespace vecdb{
+namespace vecdb {
 
-    enum class AccessMode{
-        ReadOnly,
-        ReadWrite
-    };
+enum class AccessMode { ReadOnly, ReadWrite };
 
-    class MmapArena{
-    public:
-        MmapArena(const std::filesystem::path& path, std::size_t size, AccessMode mode);
+class MmapArena {
+  public:
+    MmapArena(const std::filesystem::path &path, std::size_t size, AccessMode mode);
 
-        ~MmapArena();
+    ~MmapArena();
 
-        //remove copy constructor and copy assignment
-        MmapArena(const MmapArena&) = delete;
-        MmapArena& operator=(const MmapArena&) = delete;
+    // remove copy constructor and copy assignment
+    MmapArena(const MmapArena &) = delete;
+    MmapArena &operator=(const MmapArena &) = delete;
 
-        //explicit move constructor and move assignment
-        MmapArena(MmapArena&& other) noexcept;
-        MmapArena& operator=(MmapArena&& other) noexcept;
+    // explicit move constructor and move assignment
+    MmapArena(MmapArena &&other) noexcept;
+    MmapArena &operator=(MmapArena &&other) noexcept;
 
-        //for reading and editing data
-        std::byte* data() noexcept;
-        //for reading only
-        const std::byte* data() const noexcept;
+    // for reading and editing data
+    std::byte *data() noexcept;
+    // for reading only
+    const std::byte *data() const noexcept;
 
-        //return size of the mapped region
-        std::size_t size() const noexcept;
+    // return size of the mapped region
+    std::size_t size() const noexcept;
 
-        //grow the mapped region- existing pointers may be invalidated
-        void grow(std::size_t new_size);
+    // grow the mapped region- existing pointers may be invalidated
+    void grow(std::size_t new_size);
 
-        //flush changes to backing file
-        void flush();
+    // flush changes to backing file
+    void flush();
 
-    private:
-        struct Impl;
-        std::unique_ptr<Impl> impl_;
-
-    };
-}
+  private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+} // namespace vecdb
